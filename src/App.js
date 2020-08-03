61261cd962ea4f5620217bbf589f65d3a29b5dda
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2020-08-03 16:35:11 +0900

    테이블 형태로 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import Customer from "./components/Customer";
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell'
 
 const customers = [
   {
@@ -33,9 +38,23 @@ class App extends Component{
   render() {
     return (
         <div>
-          {customers.map(c=>{
-            return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
-          })}
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {customers.map(c=>{
+                return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+              })}
+            </TableBody>
+          </Table>
         </div>
     );
   }
