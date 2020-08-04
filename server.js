efb2e09d1790eccf3e781996aaea5834e472dc49
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2020-08-04 20:11:20 +0900

    서버 / 데이터베이스 연결

---
+++ server.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
+app.get('/api/customers',(req, res)=>{
+   connection.query(
+       'SELECT * FROM CUSTOMER',
+       (err,rows,fields)=>{
+           res.send(rows);
+       }
+   )
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
